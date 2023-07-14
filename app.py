0d798ce821073ba0228b90995908fb53eab3840b
JieunLim1 <136796436+JieunLim1@users.noreply.github.com>
2023-07-14 16:24:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,27 +12,28 @@ st.set_page_config(
 st.sidebar.success("Select a demo above.")
 
 st.markdown("""# 대학생 구원자
----
-- 이 앱은 학업에 지친 대학생에게 도움이 되고자 개발되어 필기한 내용들을 바탕으로 문제를 생성시켜 복습을 도와주는데 효율적입니다.
+---- 이 앱은 학업에 지친 대학생에게 도움이 되고자 개발되어 필기한 내용들을 바탕으로 문제를 생성시켜 복습을 도와주는데 효율적입니다.
 """)
 
-con = sq3.connect("record1.db",isolation_level = None)
+con = sq3.connect("record1.db", isolation_level = None)
 cursor = con.cursor()
 cursor.execute("SELECT context FROM ctx_data")
 row = cursor.fetchall() #[('',),('',) ...]
-raw_data = [x[0][:80] for x in row]
-print(row)
-context = list(enumerate(raw_data,1)) #[(1,'...'),(...)]
+raw_data = [x[0][:80] for x in row] #리스트 안의 각 튜플 형태로 저장되어 있는 원문을 :80까지만 출력
+#print(row)
+context = list(enumerate(raw_data,1)) #[(1,'...'),(2,'...'),(3,'...'),... ]
 
 option = st.selectbox('Which context would you like to select?', context)
-st.write('You selected:', option)
+st.write('You selected:', option) #option = (#,'...')
 
+#id, context session_state initialize
 if 'id' not in st.session_state:
     st.session_state['id'] = 0
 if 'context' not in st.session_state:
     st.session_state['context'] = ''
 st.session_state['id'] = option[0]
-st.session_state['context'] = row[option[0]]
+st.session_state['context'] = row[option[0]-1][0] #option에서 id는 1부터 시작하기 때문에 -1
+print(st.session_state['context'])
 
 
 q_type = st.radio(
@@ -65,6 +66,7 @@ if gen_button:
 
 if st.session_state['gen_button_clicked']:
     question = st.session_state['q'].show_q()
+    st.session_state['q'].q_data() #질문 저장
     st.write(question) # 질문 띄우기
 
     st.session_state['user_answer'] = st.text_area("Enter your answer : ") #사용자로부터 입력값 받기
@@ -79,5 +81,5 @@ if st.session_state['gen_button_clicked']:
         with st.spinner('Wait for it...'):
             result = st.session_state['q'].scoring(st.session_state['user_answer']) # 채점
         st.write(result) #채점한 결과 띄우기
-        #st.session_state['q'].record(st.session_state['time']) # 데이터베이스에 삽입
-        st.session_state['q'].qdata()
+        st.session_state['q'].record(st.session_state['time']) # 사용자의 답과 기타 등등 저장
+
